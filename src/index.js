b3170d55e869f5f3247ef8cf577d0f653906eb89
Yuri Alvarenga <alvarenga.hotmail.com>
2022-02-20 16:28:38 -0300

    Calculadora

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+    </div>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
